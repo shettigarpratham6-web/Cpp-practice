46e0d7dfc1c47b4cf15491e05fed65889119bf02
Pratham Shettigar <shettigarpratham6@gmail.com>
2025-09-12 10:46:34 +0530

    Add files via upload
    Printing the size and capacity of a vector.

---
+++ C++_STL_vector_size_ and_capacity.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int>vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    cout<<"size: "<<vec.size()<<endl;
+    cout<<"capacity: "<<vec.capacity()<<endl;
+
+    return 0;
+}
