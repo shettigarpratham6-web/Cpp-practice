c1c5db05c539484b31387a04e0bc81a0e9613b8d
Pratham Shettigar <shettigarpratham6@gmail.com>
2025-09-13 22:12:24 +0530

    Add files via upload
    Inserting elements into the set.

---
+++ C++_STL_sets.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main(){
+    set<int>s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(4);
+    s.insert(5);
+    for(auto val:s){
+        cout<<val<<endl;
+    }
+  cout<<endl;
+    return 0;
+}
