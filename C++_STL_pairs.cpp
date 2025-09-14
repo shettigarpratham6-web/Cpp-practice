70d312263b9c210e5241c1e0e7c9ad84d08060ad
Pratham Shettigar <shettigarpratham6@gmail.com>
2025-09-14 12:15:32 +0530

    Add files via upload

---
+++ C++_STL_pairs.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    pair<int,int>p={1,2};
+    cout<<"first element: "<<p.first;
+    cout<<endl;
+    cout<<"second element: "<<p.second;
+    cout<<endl;
+    
+    
+    return 0;
+}
